3096ea64117715d0e6690bc3a044f5d166f42409
hellonaeunkim <hellonaeunkim@gmail.com>
2025-11-11 12:57:58 +0900

    [refactor] OrderErrorCode import 추가

--- order/src/main/java/com/hubEleven/order/application/service/OrderServiceImpl.java
+++ order/src/main/java/com/hubEleven/order/application/service/OrderServiceImpl.java
@@ -1,5 +1,11 @@
 package com.hubEleven.order.application.service;
 
+import static com.hubEleven.order.domain.exception.OrderErrorCode.ORDER_NOT_FOUND;
+import static com.hubEleven.order.domain.exception.OrderErrorCode.PRODUCT_NOT_FOUND;
+import static com.hubEleven.order.domain.exception.OrderErrorCode.RECIPIENT_COMPANY_NOT_FOUND;
+import static com.hubEleven.order.domain.exception.OrderErrorCode.REQUESTOR_COMPANY_NOT_FOUND;
+import static com.hubEleven.order.domain.exception.OrderErrorCode.STOCK_RESTORE_FAILED;
+
 import com.commonLib.common.exception.GlobalException;
 import com.hubEleven.order.application.dto.OrderResult;
 import com.hubEleven.order.domain.exception.OrderErrorCode;
