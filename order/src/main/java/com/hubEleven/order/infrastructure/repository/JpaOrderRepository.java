f90dfc4f1777a58210c6f891190d7f0df68af676
hellonaeunkim <hellonaeunkim@gmail.com>
2025-11-12 20:20:33 +0900

    [chore] spotless 적용

--- order/src/main/java/com/hubEleven/order/infrastructure/repository/JpaOrderRepository.java
+++ order/src/main/java/com/hubEleven/order/infrastructure/repository/JpaOrderRepository.java
@@ -11,13 +11,13 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface JpaOrderRepository extends JpaRepository<Order, UUID> {
 
-    // keyword가 null이거나 빈 문자열인 경우 모든 주문을 반환하고, 그렇지 않은 경우 노트에 keyword가 포함된 주문을 반환
+	// keyword가 null이거나 빈 문자열인 경우 모든 주문을 반환하고, 그렇지 않은 경우 노트에 keyword가 포함된 주문을 반환
 	@Query(
 			"SELECT o FROM Order o WHERE "
 					+ "o.deletedAt IS NULL AND "
 					+ "(:keyword IS NULL OR :keyword = '' OR o.note LIKE %:keyword%)")
 	Page<Order> searchOrders(String keyword, Pageable pageable);
 
-    @Query("SELECT o FROM Order o WHERE o.orderId = :ordertId AND o.deletedAt IS NULL")
-    Optional<Order> findByIdNotDeleted(@Param("orderId") UUID orderId);
+	@Query("SELECT o FROM Order o WHERE o.orderId = :ordertId AND o.deletedAt IS NULL")
+	Optional<Order> findByIdNotDeleted(@Param("orderId") UUID orderId);
 }
--- product/src/main/java/com/hubEleven/product/infrastructure/repository/JpaProductRepository.java
+++ product/src/main/java/com/hubEleven/product/infrastructure/repository/JpaProductRepository.java
@@ -11,14 +11,15 @@ import org.springframework.data.repository.query.Param;
 
 public interface JpaProductRepository extends JpaRepository<Product, UUID> {
 
-    boolean existsByCompanyIdAndNameAndHubId(UUID companyId, String name, UUID hubId);
+	boolean existsByCompanyIdAndNameAndHubId(UUID companyId, String name, UUID hubId);
 
-    // keyword가 null인 경우 모든 제품을 반환하고, 그렇지 않은 경우 이름에 keyword가 포함된 제품을 반환
-    @Query("SELECT p FROM Product p WHERE " +
-            "(:keyword IS NULL OR p.name LIKE %:keyword%) " +
-            "AND p.deletedAt IS NULL")
+	// keyword가 null인 경우 모든 제품을 반환하고, 그렇지 않은 경우 이름에 keyword가 포함된 제품을 반환
+	@Query(
+			"SELECT p FROM Product p WHERE "
+					+ "(:keyword IS NULL OR p.name LIKE %:keyword%) "
+					+ "AND p.deletedAt IS NULL")
 	Page<Product> searchProducts(@Param("keyword") String keyword, Pageable pageable);
 
-    @Query("SELECT p FROM Product p WHERE p.productId = :productId AND p.deletedAt IS NULL")
-    Optional<Product> findByIdNotDeleted(@Param("productId") UUID productId);
+	@Query("SELECT p FROM Product p WHERE p.productId = :productId AND p.deletedAt IS NULL")
+	Optional<Product> findByIdNotDeleted(@Param("productId") UUID productId);
 }
