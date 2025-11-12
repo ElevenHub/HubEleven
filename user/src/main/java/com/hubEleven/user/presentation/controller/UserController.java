75f3aac21635575dfe9b0513fcfb428f5b7fa2b3
Doritosch <als000825@naver.com>
2025-11-12 15:47:39 +0900

    fix : preauthorize 제거

--- user/src/main/java/com/hubEleven/user/presentation/controller/UserController.java
+++ user/src/main/java/com/hubEleven/user/presentation/controller/UserController.java
@@ -77,7 +77,6 @@ public class UserController {
 	}
 
 	@GetMapping
-	@PreAuthorize("hasAnyAuthority('ROLE_MASTER', 'ROLE_HUB_MANAGER')")
 	public ResponseEntity<ApiResponse<CommonPageResponse<UserInfoResponse>>> getAllUsers(
 			@Valid CommonPageRequest pageRequest) {
 
@@ -118,7 +117,6 @@ public class UserController {
 	}
 
 	@PatchMapping("/{id}/status")
-	@PreAuthorize("hasAnyAuthority('ROLE_MASTER', 'ROLE_HUB_MANAGER')")
 	public ResponseEntity<ApiResponse<Void>> updateUserStatus(
 			@PathVariable("id") Long id, @Valid @RequestBody UserStatusUpdateRequest request) {
 		UserStatusUpdateCommand command = new UserStatusUpdateCommand(id, request.status());
@@ -128,7 +126,6 @@ public class UserController {
 	}
 
 	@GetMapping("/search")
-	@PreAuthorize("hasAnyAuthority('ROLE_MASTER', 'ROLE_HUB_MANAGER')")
 	public ResponseEntity<ApiResponse<CommonPageResponse<UserInfoResponse>>> searchUsers(
 			@Valid CommonPageRequest pageRequest) {
 		CommonPageResponse<UserInfo> result = userService.searchUsers(pageRequest);
@@ -147,7 +144,6 @@ public class UserController {
 	}
 
 	@PutMapping("/{id}")
-	@PreAuthorize("hasAuthority('ROLE_MASTER')")
 	public ResponseEntity<ApiResponse<UserInfoResponse>> updateUser(
 			@PathVariable("id") Long id, @Valid @RequestBody UserUpdateRequest request) {
 		UserUpdateCommand command =
@@ -165,7 +161,6 @@ public class UserController {
 	}
 
 	@DeleteMapping("/{id}")
-	@PreAuthorize("hasAuthority('ROLE_MASTER')")
 	public ResponseEntity<ApiResponse<Void>> deleteUser(
 			@PathVariable("id") Long id, @RequestHeader("X-User-Id") Long requestUserId) {
 		userService.deleteUser(id, requestUserId);
