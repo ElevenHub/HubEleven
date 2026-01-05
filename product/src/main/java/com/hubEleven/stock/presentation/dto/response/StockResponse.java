8c81f0f24d5ca8d47d1f182092492a693d59a1ae
hellonaeunkim <hellonaeunkim@gmail.com>
2026-01-05 17:18:05 +0900

    chore : 코드 포맷팅(spotlessApply) 적용

--- product/src/main/java/com/hubEleven/stock/presentation/dto/response/StockResponse.java
+++ product/src/main/java/com/hubEleven/stock/presentation/dto/response/StockResponse.java
@@ -4,16 +4,15 @@ import com.hubEleven.stock.application.dto.StockResult;
 import java.util.UUID;
 
 public record StockResponse(
-        UUID stockId, UUID productId, String productName, UUID hubId, UUID companyId,
-        int quantity) {
+		UUID stockId, UUID productId, String productName, UUID hubId, UUID companyId, int quantity) {
 
-    public static StockResponse from(StockResult stockResult) {
-        return new StockResponse(
-                stockResult.stockId(),
-                stockResult.productId(),
-                stockResult.productName(),
-                stockResult.hubId(),
-                stockResult.companyId(),
-                stockResult.quantity());
-    }
+	public static StockResponse from(StockResult stockResult) {
+		return new StockResponse(
+				stockResult.stockId(),
+				stockResult.productId(),
+				stockResult.productName(),
+				stockResult.hubId(),
+				stockResult.companyId(),
+				stockResult.quantity());
+	}
 }
