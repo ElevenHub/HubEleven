df038e4a14538de4bed64556d22d1e9e19b661d3
S2hyeyunS2 <hhyeyun22@gmail.com>
2026-01-06 23:39:50 +0900

    [chore] 코드포맷팅 적용

--- company/src/main/java/com/hubEleven/company/presentation/dto/request/ChangeCompanyStatusRequest.java
+++ company/src/main/java/com/hubEleven/company/presentation/dto/request/ChangeCompanyStatusRequest.java
@@ -4,6 +4,4 @@ import com.hubEleven.company.domain.vo.CompanyStatus;
 import jakarta.validation.constraints.NotNull;
 
 public record ChangeCompanyStatusRequest(
-		@NotNull(message = "업체 상태는 필수 입력 항목입니다.")
-		CompanyStatus status) {
-}
+		@NotNull(message = "업체 상태는 필수 입력 항목입니다.") CompanyStatus status) {}
--- company/src/main/java/com/hubEleven/company/presentation/dto/request/CreateCompanyRequest.java
+++ company/src/main/java/com/hubEleven/company/presentation/dto/request/CreateCompanyRequest.java
@@ -7,19 +7,9 @@ import jakarta.validation.constraints.Size;
 import java.util.UUID;
 
 public record CreateCompanyRequest(
-		@NotNull(message = "허브 ID는 필수 입력 항목입니다.")
-		UUID hubId,
-		
-		@NotBlank(message = "업체명은 필수 입력 항목입니다.")
-		@Size(max = 150, message = "업체명은 150자 이하여야 합니다.")
-		String name,
-		
-		@NotNull(message = "업체 타입은 필수 입력 항목입니다.")
-		CompanyType type,
-		
-		@Size(max = 100, message = "Slack ID는 100자 이하여야 합니다.")
-		String slackId,
-		
-		@Size(max = 300, message = "주소는 300자 이하여야 합니다.")
-		String address) {
-}
+		@NotNull(message = "허브 ID는 필수 입력 항목입니다.") UUID hubId,
+		@NotBlank(message = "업체명은 필수 입력 항목입니다.") @Size(max = 150, message = "업체명은 150자 이하여야 합니다.")
+				String name,
+		@NotNull(message = "업체 타입은 필수 입력 항목입니다.") CompanyType type,
+		@Size(max = 100, message = "Slack ID는 100자 이하여야 합니다.") String slackId,
+		@Size(max = 300, message = "주소는 300자 이하여야 합니다.") String address) {}
