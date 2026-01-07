d20d93278ace887e857bdef429dc9d98636229e1
Robbie <Robbie@email.com>
2026-01-07 19:14:21 +0900

    Security 리팩토링

--- delivery/src/main/java/com/hubEleven/delivery/infrastructure/security/SecurityConfig.java
+++ delivery/src/main/java/com/hubEleven/delivery/infrastructure/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -12,30 +13,31 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
 
-	private final HeaderAuthenticationFilter headerAuthenticationFilter;
+    private final HeaderAuthenticationFilter headerAuthenticationFilter;
 
-	public SecurityConfig(HeaderAuthenticationFilter headerAuthenticationFilter) {
-		this.headerAuthenticationFilter = headerAuthenticationFilter;
-	}
+    public SecurityConfig(HeaderAuthenticationFilter headerAuthenticationFilter) {
+        this.headerAuthenticationFilter = headerAuthenticationFilter;
+    }
 
-	@Bean
-	@Order(1)
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable())
-				.authorizeHttpRequests(
-						authz ->
-								authz
-										.requestMatchers(
-												"/v3/api-docs/**",
-												"/swagger-ui/**",
-												"/swagger-ui.html",
-												"/swagger-resources/**",
-												"/webjars/**")
-										.permitAll()
-										.anyRequest()
-										.authenticated())
-				.addFilterBefore(headerAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    @Bean
+    @Order(1)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(
+                authz ->
+                    authz
+                        .requestMatchers(
+                            "/v3/api-docs/**",
+                            "/swagger-ui/**",
+                            "/swagger-ui.html",
+                            "/swagger-resources/**",
+                            "/webjars/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+            .addFilterBefore(headerAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class);
 
-		return http.build();
-	}
+        return http.build();
+    }
 }
