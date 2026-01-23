5ce6ee91566b5f0f754c41653f136f6d470ac686
S2hyeyunS2 <hhyeyun22@gmail.com>
2026-01-23 21:12:38 +0900

    [del] 불필요한 SlackWebhook 삭제

--- notification/src/main/java/com/hubEleven/notification/slack/infrastructure/config/SlackProperties.java
+++
@@ -1,6 +0,0 @@
-package com.hubEleven.notification.slack.infrastructure.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "slack")
-public record SlackProperties(String webhookUrl) {}
